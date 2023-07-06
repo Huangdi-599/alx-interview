546b8a1cd109b9bad9aed628730516d4c9c23733
Huangdi-599 <92282845+Huangdi-599@users.noreply.github.com>
2023-07-06 23:50:21 +0100

    ahmed

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def can_unlock_all(boxes):
+    """
+    Determine if all the boxes can be opened.
+
+    Args:
+        boxes (List[List[int]]): A list of lists representing the boxes and their corresponding keys.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+
+    n = len(boxes)  # Number of boxes
+    unlocked = [False] * n  # Track the locked/unlocked status of each box
+    unlocked[0] = True  # The first box is initially unlocked
+
+    # Iterate through each box and check its keys
+    for box in range(n):
+        if unlocked[box]:
+            for key in boxes[box]:
+                if key < n:  # Ignore keys that don't correspond to a box
+                    unlocked[key] = True
+
+    # Check if all boxes are unlocked
+    return all(unlocked)
